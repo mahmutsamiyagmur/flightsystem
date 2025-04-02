76109d428e395ba200beeb7637e45519eb44f25f
mahmutyagmur <mahmut@starfish.team>
2025-04-02 12:49:20 +0300

    init commit.

---
+++ src/test/java/com/msy/projects/flightsystem/FlightsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msy.projects.flightsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
